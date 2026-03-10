573948ff42495a57a7f5368c06dcecc2c5c3b3a6
milanKUN <milankundu2003@gmail.com>
2026-03-10 16:51:48 +0530

    ci cd chaking

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -37,7 +37,7 @@ export default function Navbar() {
           <div className="flex items-center gap-10">
             <Link to="/" className="nav-item cursor-pointer">
               <span className="nav-pill"></span>
-              <span className="nav-text">Home</span>
+              <span className="nav-text">Home Milan</span>
             </Link>
 
             <Link to="/marketplace" className="nav-item cursor-pointer">
