63d4d74dd5464184c3eb2baa99ea28f38590b385
ChanchalBansal <chanchal2895@gmail.com>
2026-03-18 14:20:42 +0530

    portfolio sub table update

---
+++ bcakend_api/database/migrations/2026_03_18_080506_alter_portfolio_projects_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('portfolio_projects', function (Blueprint $table): void {
+			$table->id()->primary()->autoIncrement()->change();
+			$table->foreignId('portfolio_id')->constrained('portfolios')->cascadeOnDelete()->change();
+            $table->index('portfolio_id')->change();
+		});
+        Schema::table('portfolio_media', function (Blueprint $table): void {
+			$table->id()->primary()->autoIncrement()->change();
+			$table->foreignId('project_id')->constrained('portfolio_projects')->cascadeOnDelete();
+            $table->index('project_id')->change();
+		});
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('portfolio_projects', function (Blueprint $table): void {
+			$table->id()->primary()->autoIncrement()->change();
+			$table->foreignId('portfolio_id')->constrained('portfolios')->cascadeOnDelete()->change();
+            $table->index('portfolio_id')->change();
+		});
+        Schema::table('portfolio_media', function (Blueprint $table): void {
+			$table->id()->primary()->autoIncrement()->change();
+			$table->foreignId('project_id')->constrained('portfolio_projects')->cascadeOnDelete();
+            $table->index('project_id')->change();
+		});
+    }
+};
