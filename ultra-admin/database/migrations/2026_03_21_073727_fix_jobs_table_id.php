8477df05d5b4d1d2b86bc27fcc317f16ae23aa63
ChanchalBansal <chanchal2895@gmail.com>
2026-03-21 13:09:36 +0530

    jobs

--- ultra-admin/database/migrations/2026_03_21_073727_fix_jobs_table_id.php
+++ ultra-admin/database/migrations/2026_03_21_073727_fix_jobs_table_id.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('jobs', function (Blueprint $table) {
-            $table->id()->change();
+            $table->primary('id')->change();
         });
     }
 
