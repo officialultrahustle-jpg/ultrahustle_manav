dc35e1bd04689414f3d98542b42ff3cde18adbc9
ChanchalBansal <chanchal2895@gmail.com>
2026-04-04 17:35:49 +0530

    user public private profile

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -93,7 +93,7 @@ export default function App() {
           <Route path="/edit-team/:teamId" element={<EditTeam theme={theme} setTheme={setTheme} />} />
           <Route path="/team-profile/:username" element={<TeamProfileLight theme={theme} setTheme={setTheme} />} />
           <Route path="/public-team-profile/:username" element={<PublicTeamProfile theme={theme} setTheme={setTheme} />} />
-          <Route path="/user-profile/" element={<ProtectedRoute><UserProfile theme={theme} setTheme={setTheme} /></ProtectedRoute>} />
+          <Route path="/user-profile" element={<ProtectedRoute><UserProfile theme={theme} setTheme={setTheme} /></ProtectedRoute>} />
           <Route path="/public-user-profile/:username" element={<PublicUserProfile theme={theme} setTheme={setTheme} />} />
           <Route path="/contracts-listing" element={<SoloContractListing theme={theme} setTheme={setTheme} />} />
           <Route path="/milestones" element={<MilestonesPage theme={theme} setTheme={setTheme} />} />
