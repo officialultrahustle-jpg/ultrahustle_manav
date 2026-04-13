ddf4f02670cbfc91ed101adf2af06d047c5b94db
ChanchalBansal <chanchal2895@gmail.com>
2026-04-13 12:20:28 +0530

    listing view

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -416,22 +416,22 @@ export default function App() {
                             <CourseListing theme={theme} setTheme={setTheme} />
                         }
                     />
-                    <Route
+                   {/*  <Route
                         path="/digital-product/:username"
                         element={<ProductDeliverables />}
                     />
                     <Route
                         path="/course/:username"
                         element={<CourseDeliverables />}
-                    />
+                    /> */}
                     <Route
                         path="/edit-course/:username"
                         element={<EditCourse />}
                     />
-                    <Route
+                    {/* <Route
                         path="/webinar/:username"
                         element={<WebinarDeliverables />}
-                    />
+                    /> */}
                     <Route
                         path="/edit-webinar/:username"
                         element={<EditWebinar />}
